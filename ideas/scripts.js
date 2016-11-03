9c52df5ad6733b76206eea0068461483273c4fec
nrwinner <WinnerDigital@host.local>
2016-11-03 13:51:48 -0400

    demo stuff

---
+++ ideas/scripts.js
@@ -0,0 +1,82 @@
+var voting = false;
+
+jQuery(document).ready(function($) {
+    $(".upvote").on("click", function() {
+        if (!voting) {
+            $(this).siblings(".fa-spinner").addClass("open");
+            voting = true;
+            if ($(this).hasClass("me")) {
+                var elem = $(this);
+
+                $.post("../helpers/ideas/remove_upvote.php", {idea: $(this).parents(".idea").data("idea")}, function(response) {
+                    if (response > 0) {
+                        $(elem).removeClass("me");
+                        $(".vote_count", elem).html(parseInt($(".vote_count", elem).html()) - 1);
+                    }
+                    $(this).siblings(".fa-spinner").removeClass("open");
+                });
+            } else if ($(this).parents(".idea").hasClass("mine")) {
+                alert("You can't upvote your own posts!");
+                $(elem).siblings(".fa-spinner").removeClass("open");
+            } else {
+                var elem = $(this);
+                $.post("../helpers/ideas/upvote.php", {idea: $(this).parents(".idea").data("idea")}, function(response) {
+                    if (response > 0) {
+                        var up_count = $(".idea[data-idea=" + response + "] .vote .upvote .vote_count").html();
+                        $(".idea[data-idea=" + response + "] .vote .upvote .vote_count").html(parseInt(up_count) + 1);
+
+                        var down_count = $(".idea[data-idea=" + response + "] .vote .downvote .vote_count").html();
+                        if ($(".idea[data-idea=" + response + "] .vote .downvote").hasClass("me"))
+                            $(".idea[data-idea=" + response + "] .vote .downvote .vote_count").html(parseInt(down_count) - 1);
+
+                        $(".idea[data-idea=" + response + "] .vote div").removeClass("me");
+                        $(".idea[data-idea=" + response + "] .vote .upvote").addClass("me");
+                    }
+                    voting = false;
+                    $(elem).siblings(".fa-spinner").removeClass("open");
+                });
+            }
+
+            if (voting) voting = false;
+        }
+    });
+
+    $(".downvote").on("click", function() {
+        if (!voting) {
+            var elem = $(this);
+            $(this).siblings(".fa-spinner").addClass("open");
+            voting = true;
+            if ($(this).hasClass("me")) {
+
+                $.post("../helpers/ideas/remove_downvote.php", {idea: $(this).parents(".idea").data("idea")}, function(response) {
+                    if (response > 0) {
+                        $(elem).removeClass("me");
+                        $(".vote_count", elem).html(parseInt($(".vote_count", elem).html()) - 1);
+                    }
+                    $(elem).siblings(".fa-spinner").removeClass("open");
+                });
+            } else if ($(this).parents(".idea").hasClass("mine")) {
+                alert("You can't upvote your own posts!");
+                $(elem).siblings(".fa-spinner").removeClass("open");
+            } else {
+                $.post("../helpers/ideas/downvote.php", {idea: $(this).parents(".idea").data("idea")}, function(response) {
+                    if (response > 0) {
+                        var up_count = $(".idea[data-idea=" + response + "] .vote .upvote .vote_count").html();
+                        if (up_count > 0 && $(".idea[data-idea=" + response + "] .vote .upvote").hasClass("me"))
+                            $(".idea[data-idea=" + response + "] .vote .upvote .vote_count").html(parseInt(up_count) - 1);
+
+                        var down_count = $(".idea[data-idea=" + response + "] .vote .downvote .vote_count").html();
+                        $(".idea[data-idea=" + response + "] .vote .downvote .vote_count").html(parseInt(down_count) + 1);
+
+                        $(".idea[data-idea=" + response + "] .vote div").removeClass("me");
+                        $(".idea[data-idea=" + response + "] .vote .downvote").addClass("me");
+                    }
+                    voting = false;
+                    $(elem).siblings(".fa-spinner").removeClass("open");
+                });
+            }
+
+            if (voting) voting = false;
+        }
+    });
+})
